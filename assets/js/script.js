fa922b6654c2a53e9a1997848fef1813c299884b
l-amu <lamiekgim@gmail.com>
2021-10-01 20:14:18 -0400

    relaod fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ console.log(formEl);
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-}
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+
 
 formEl.addEventListener("submit", createTaskHandler);
